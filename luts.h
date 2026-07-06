bc2431a040c1e87244b9daa6366faeb71be6d340
Igor Glajcher <kasetonix@gmail.com>
2026-07-06 23:23:33 +0200

    [06.07.2026] Added the codeword num lookup table.

--- luts.h
+++ luts.h
@@ -10,5 +10,6 @@
 typedef uint16_t u16;
 extern char alphanumeric_special[ALPHANUMERIC_SPECIAL_NUM];
 extern u16 qrcode_capacity[VERSION_NUM][ECC_LEVEL_NUM][MODE_NUM];
+extern u16 codeword_num[VERSION_NUM][ECC_LEVEL_NUM][2];
 
 #endif

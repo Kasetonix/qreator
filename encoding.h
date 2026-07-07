57b623707fe86e65b8e938e74133cc349a10f022
Igor Glajcher <kasetonix@gmail.com>
2026-07-07 03:17:02 +0200

    [07.07.2026] Finalised the encoding function.

--- encoding.h
+++ encoding.h
@@ -39,6 +39,6 @@ typedef enum ECC_Level { ECC_L, ECC_M, ECC_Q, ECC_H } ECC_Level;
 
 Mode get_encoding_mode(String text);
 u8 get_version(String text, Mode mode, ECC_Level ecc_level);
-Array_u8 encode(String text, u8 version, Mode mode, ECC_Level ecc_level);
+void encode(String text, u8 version, Mode mode, ECC_Level ecc_level, Array_u16 *encoding, u8 **word_lengths);
 
 #endif
--- qreator.c
+++ qreator.c
@@ -4,37 +4,38 @@
 #include <stdint.h>
 
 #include "encoding.h"
-#include "qrcode_gen.h"
-#include "vis.h"
+// #include "qrcode_gen.h"
+// #include "vis.h"
 
 int main(void) {
-    u8 qrcode_version;
-    QR_Code qrcode;
     String text;
-    Array_u8 encoding;
+    u8 qrcode_version;
     Mode encoding_mode;
+    // QR_Code qrcode;
+    Array_u16 encoding;
+    u8 *word_lengths;
 
     // fputs("Enter the message: ", stdout);
     read(&text);
 
     encoding_mode = get_encoding_mode(text);
     qrcode_version = get_version(text, encoding_mode, ECC_Q);
+    encode(text, qrcode_version, encoding_mode, ECC_Q, &encoding, &word_lengths);
 
-    encoding = encode(text, qrcode_version, encoding_mode, ECC_Q);
+    printf("Mode:   %0*b\n", word_lengths[0], encoding.elems[0]);
+    printf("Length: %0*b\n", word_lengths[1], encoding.elems[1]);
+    puts("Data:");
+    for (size_t i = 2; i < encoding.len; i++) {
+        printf("%0*b\n", word_lengths[i], encoding.elems[i]);
+    }
 
-    // for (size_t i = 0; i < encoding.len; i++) {
-    //     printf("%08b ", encoding.elems[i]);
-    // } putchar('\n');
-
-    return 0;
-
-    init_qrcode(&qrcode, qrcode_version, encoding_mode, ECC_Q);
-    alloc_qrcode(&qrcode);
-    create_qrcode_blueprint(&qrcode);
-    draw_qrcode_small(&qrcode);
+    // init_qrcode(&qrcode, qrcode_version, encoding_mode, ECC_Q);
+    // alloc_qrcode(&qrcode);
+    // create_qrcode_blueprint(&qrcode);
+    // draw_qrcode_small(&qrcode);
 
     free(text.chars);
     free(encoding.elems);
-    free(qrcode.matrix);
+    // free(qrcode.matrix);
     return 0;
 }

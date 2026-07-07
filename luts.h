caa28f1a902863ed173595d308c338bb1891e8b8
Igor Glajcher <kasetonix@gmail.com>
2026-07-08 00:29:55 +0200

    [08.07.2026] Added LUT containing block divisions.

--- luts.h
+++ luts.h
@@ -7,9 +7,16 @@
 #define ECC_LEVEL_NUM 4
 #define MODE_NUM 3
 
+#define IND_G1_BLOCKS 1
+#define IND_G1_CODEWORD_PER_BLOCK 2
+#define IND_G2_BLOCKS 3
+#define IND_G2_CODEWORD_PER_BLOCK 4
+
+typedef uint8_t u8;
 typedef uint16_t u16;
 extern char alphanumeric_special[ALPHANUM_SPECIAL_NUM];
 extern u16 qrcode_capacity[VERSION_NUM][ECC_LEVEL_NUM][MODE_NUM];
 extern u16 data_codeword_num[VERSION_NUM][ECC_LEVEL_NUM];
+extern u8 block_division[VERSION_NUM][ECC_LEVEL_NUM][4];
 
 #endif
